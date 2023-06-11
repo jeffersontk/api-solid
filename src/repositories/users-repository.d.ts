351e2f9e132746ba0280587ccd9b61486bc6ba39
jefferson brito <jscabral@firjan.com.br>
2023-06-11 11:30:16 -0300

    subindo projeto de api com solid

---
+++ src/repositories/users-repository.d.ts
@@ -0,0 +1,6 @@
+import { Prisma, User } from "@prisma/client"
+
+interface UsersRepository {
+  create(data: Prisma.UserCreateInput): Promise<User>
+  findByEmail(email: string): Promise<User | null>
+}
